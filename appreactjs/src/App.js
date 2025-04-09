64c58f8ed8520c919b857523d9967b9093a8a8e9
fernandoifto <fernando.algaworks@gmail.com>
2025-04-09 19:39:42 -0300

    criação de componente lista de pessoas

--- appreactjs/src/App.js
+++ appreactjs/src/App.js
@@ -1,12 +1,23 @@
 import ListaPessoas from "./components/ListaPessoas";
 
 function App() {
+  const pessoas = [
+    { nome: "Gabriela", idade: "18" },
+    { nome: "Ana", idade: "30" },
+    { nome: "Rafaele", idade: "21" },
+    { nome: "Maria", idade: "40" },
+    { nome: "Felipe", idade: "42" }
+  ];
 
   return (
     <div>
-      <ListaPessoas/>
-      <ListaPessoas/>
-      <ListaPessoas/>
+      {pessoas.map((pessoa, i) => (
+        <ListaPessoas 
+          key={i} // Adiciona uma chave única para cada item da lista
+          nome={pessoa.nome}
+          idade={pessoa.idade}
+        />
+      ))}
     </div>
   );
 }
--- appreactjs/src/components/ListaPessoas.jsx
+++ appreactjs/src/components/ListaPessoas.jsx
@@ -1,16 +1,8 @@
-/*const pessoas = [
-    { nome: "Gabriela", idade: "18"},
-    { nome: "Ana", idade: "30"},
-    { nome: "Rafaele", idade: "21"},
-    { nome: "Maria", idade: "40"},
-    { nome: "Felipe", idade: "42"}
-];*/
-
-const ListaPessoas = () => {
+const ListaPessoas = ({nome, idade}) => {
     return (
         <div>
-          <h2>Meu nome: </h2>
-          <h2>Idade: </h2>
+          <h2>Meu nome: {nome}</h2>
+          <h2>Idade: {idade} </h2>
         </div>
     );
 }
