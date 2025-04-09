358009911607d1e451b1bb8ef7415cde46115b58
fernandoifto <fernando.algaworks@gmail.com>
2025-04-09 11:32:17 -0300

    criação readme

---
+++ appreactjs/src/App.js
@@ -0,0 +1,14 @@
+import ListaPessoas from "./components/ListaPessoas";
+
+function App() {
+
+  return (
+    <div>
+      <ListaPessoas/>
+      <ListaPessoas/>
+      <ListaPessoas/>
+    </div>
+  );
+}
+
+export default App;
---
+++ appreactjs/src/components/ListaPessoas.jsx
@@ -0,0 +1,18 @@
+/*const pessoas = [
+    { nome: "Gabriela", idade: "18"},
+    { nome: "Ana", idade: "30"},
+    { nome: "Rafaele", idade: "21"},
+    { nome: "Maria", idade: "40"},
+    { nome: "Felipe", idade: "42"}
+];*/
+
+const ListaPessoas = () => {
+    return (
+        <div>
+          <h2>Meu nome: </h2>
+          <h2>Idade: </h2>
+        </div>
+    );
+}
+
+export default ListaPessoas
